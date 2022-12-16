8f032aed6d4b82d450d61b70c218f09b866a78f3
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-16 12:42:39 -0700

    add last five tests to ContactForm.test.js

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -30,21 +30,46 @@ test('renders THREE error messages if user enters no values into any fields.', a
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-
+    render(<ContactForm />);
+    userEvent.type(screen.getByPlaceholderText('Edd'), 'Marcus');
+    userEvent.type(screen.getByPlaceholderText('Burke'), 'Stephens');
+    userEvent.click(screen.getByText(/submit/i));
+    expect(screen.getByText(/Error: email must be a valid email address./i)).toBeInTheDocument();
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-
+    render(<ContactForm />);
+    userEvent.type(screen.getByPlaceholderText('bluebill1049@hotmail.com'), 'Marcus');
+    expect(screen.getByText(/Error: email must be a valid email address./i)).toBeInTheDocument();
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-
+    render(<ContactForm />);
+    userEvent.type(screen.getByPlaceholderText('Edd'), 'Marcus');
+    userEvent.type(screen.getByPlaceholderText('bluebill1049@hotmail.com'), 'kyle@kyle.com');
+    userEvent.click(screen.getByText(/submit/i));
+    expect(screen.getByText(/lastname is a required field/i));
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-
+    render(<ContactForm />);
+    userEvent.type(screen.getByPlaceholderText('Edd'), 'Marcus');
+    userEvent.type(screen.getByPlaceholderText('Burke'), 'Stephens');
+    userEvent.type(screen.getByPlaceholderText('bluebill1049@hotmail.com'), 'kyle@kyle.com');
+    userEvent.click(screen.getByText(/submit/i));
+    expect(screen.getByTestId('firstnameDisplay')).toBeInTheDocument();
+    expect(screen.getByTestId('lastnameDisplay')).toBeInTheDocument();
+    expect(screen.getByTestId('emailDisplay')).toBeInTheDocument();
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
-
+    render(<ContactForm />);
+    userEvent.type(screen.getByPlaceholderText('Edd'), 'Marcus');
+    userEvent.type(screen.getByPlaceholderText('Burke'), 'Stephens');
+    userEvent.type(screen.getByPlaceholderText('bluebill1049@hotmail.com'), 'kyle@kyle.com');
+    userEvent.click(screen.getByText(/submit/i));
+    expect(screen.getByTestId('firstnameDisplay')).toBeInTheDocument();
+    expect(screen.getByTestId('lastnameDisplay')).toBeInTheDocument();
+    expect(screen.getByTestId('emailDisplay')).toBeInTheDocument();
+    expect(screen.getByTestId('messageDisplay')).toBeInTheDocument();
 });
